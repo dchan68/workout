4fb14c8bb86783c6228f570e008e028b8570728b
Daryan Chan <daryan.j.chan@hotmail.com>
2025-03-10 15:22:59 -0400

    fetching data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
 
 function Home() {
+  const [workouts, setWorkouts] = useState(null);
+  useEffect(function () {
+    async function fetchWorkout() {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      console.log("The response for getting all the workouts " + response);
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    }
+    fetchWorkout();
+  }, []);
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <h2 className="workouts">
+        {workouts &&
+          workouts.map(function (workout) {
+            return <WorkoutDetails key={workout._id} workout={workout} />;
+          })}
+      </h2>
     </div>
   );
 }
